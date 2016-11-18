32ef78f5014a2e9f6f63a36f3e51d2d0605163e3
Jan Henning Thorsen <jhthorsen@cpan.org>
2016-11-18 14:38:35 +0100

    Released version 0.04
     - Improved error handling in SolarBeam::Respons

--- lib/SolarBeam.pm
+++ lib/SolarBeam.pm
@@ -8,7 +8,7 @@ use SolarBeam::Query;
 use SolarBeam::Response;
 use SolarBeam::Util 'escape';
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 has ua            => sub { Mojo::UserAgent->new };
 has default_query => sub { {} };
@@ -193,7 +193,7 @@ SolarBeam - Async Solr search driver
 
 =head1 VERSION
 
-0.03
+0.04
 
 =head1 SYNOPSIS
 
